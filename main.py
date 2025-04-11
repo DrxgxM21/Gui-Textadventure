ca481e4d4c08fbe53b5fe70c6959917463e36972
DrxgxM21 <151265522+DrxgxM21@users.noreply.github.com>
2025-04-11 22:34:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Main entry point for the game application.
+"""
+
+from Modules import GUImodules as Gk
+
+if __name__ == "__main__":
+    app = Gk.GUI()
+    app.mainloop()  # Start the GUI event loop
